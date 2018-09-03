895553ff0ff01191290a721d7f4886b28be5f041
Jason Xiao <jaesonxh@gmail.com>
2018-09-04 00:58:27 +1000

    added populating list view and started on payment window

---
+++ POS_SYSTEM/POS_SYSTEM/orderInfoWindow.xaml.vb
@@ -0,0 +1,43 @@
+﻿Imports OrderSoft
+
+Public Class POSDish
+    Public Property dishId As Integer
+    Public Property name As String
+    Public Property size As String
+    Public Property upgradePrice As Single
+    Public Property totalPrice As Single
+End Class
+
+Public Class POSOrder
+    Public Property origOrder As OrderObject
+    Public Property orderId As String
+    Public Property tableNumber As Integer
+    Public Property serverID As String
+
+
+
+    Public Property subtotal As Single
+    Public Property tax As Single
+    Public Property orderTotal As Single
+End Class
+
+Public Class orderInfoWindow
+
+    Dim orderClient As OSClient
+
+    Public Sub New(client, order)
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        orderClient = client
+        initialiseWindow()
+
+    End Sub
+
+    ' Calculates and adds all required info in the window
+    Public Async Sub initialiseWindow()
+        Dim originalOrder = Await orderClient.GetOrder()
+    End Sub
+End Class
--- POS_SYSTEM/POS_SYSTEM/posWindow.xaml.vb
+++ POS_SYSTEM/POS_SYSTEM/posWindow.xaml.vb
@@ -4,47 +4,69 @@ Imports System.Windows.Threading
 ' list box stuff https://www.tutorialspoint.com/vb.net/vb.net_listbox.htm
 
 Public Class basicOrder
+    Public Property OrderId As String
     Public Property TableNumber As String
     Public Property TimeSubmitted As String
     Public Property TimeCompleted As String
 End Class
 
 Public Class posWindow
+
+    Dim ordersInListView As New List(Of String) ' A list of OrderIDs keeping track of what orders are already in the list view
+    Dim selectedOrder As String ' OrderID of currently selected order in list view
+
     Dim posClient As OSClient
-    Private posTimer As DispatcherTimer
+    Private posTimer As DispatcherTimer ' Timer to refresh unpaid orders every tick
+
+
     Public Sub New(client)
         ' This call is required by the designer.
         InitializeComponent()
 
         ' Add any initialization after the InitializeComponent() call.
-
         ' Passing through client from loginWindow
         posClient = client
 
         ' Timer setup
         posTimer = New DispatcherTimer
-        posTimer.Interval = TimeSpan.FromMilliseconds(3000)
-        AddHandler posTimer.Tick, AddressOf posTick
+        posTimer.Interval = TimeSpan.FromMilliseconds(3000) ' Tick every 3 seconds
+        AddHandler posTimer.Tick, AddressOf PosTick
         posTimer.Start()
 
-        ' Orders listview setup
-
+        ' Initial update of the list view
+        PosTick() ' Initial tick
     End Sub
 
-    ' Executes every 3 seconds
-    Private Async Sub posTick()
-        lvOrders.Items.Clear()
-        ' Ask server for unpaid orders and updates the list view
-        Dim openOrderIds = Await posClient.GetUnpaidOrders()
-        For Each id As String In openOrderIds
+    ' A timer that requests from the server what orders are currently unpaid
+    ' Populates the list view with the unpaid orders
+    Private Async Sub PosTick()
+        ' Ask server for unpaid orders
+        Dim unpaidOrderIds = Await posClient.GetUnpaidOrders()
+
+        ' Loop through all unpaid orders
+        For Each id As String In unpaidOrderIds
             Dim newOrder = Await posClient.GetOrder(orderId:=id)
             Dim valuesToAdd As New basicOrder
 
+            valuesToAdd.OrderId = newOrder.OrderId
             valuesToAdd.TableNumber = Convert.ToString(newOrder.TableNumber)
             valuesToAdd.TimeSubmitted = newOrder.TimeSubmittedString
             valuesToAdd.TimeCompleted = newOrder.TimeCompletedString
-            lvOrders.Items.Add(valuesToAdd)
+
+            If ordersInListView.Contains(valuesToAdd.OrderId) = False Then
+                lvOrders.Items.Add(valuesToAdd)
+                ordersInListView.Add(newOrder.OrderId)
+            End If
+
         Next
     End Sub
 
+    Private Sub btnPay_Click(sender As Object, e As RoutedEventArgs) Handles btnPay.Click
+        Dim nextWindow As orderInfoWindow = New orderInfoWindow(posClient, selectedOrder)
+        nextWindow.Show()
+    End Sub
+
+    Private Sub lvOrders_SelectionChanged(sender As Object, e As SelectionChangedEventArgs) Handles lvOrders.SelectionChanged
+        selectedOrder = lvOrders.SelectedItem.OrderId
+    End Sub
 End Class
